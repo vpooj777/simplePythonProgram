11c098be833f55cb0438336dffcca66bd9a68741
vpooj777 <vpooj@eg.dk>
2025-02-23 13:40:53 +0530

    Delete ListToFile.py

--- ListToFile.py
+++
@@ -1,7 +0,0 @@
-#write a list to file
-color = ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
-with open('abc.txt', "w") as myfile:
-        myfile.write("\n".join(color))
-
-content = open('abc.txt')
-print(content.read())
